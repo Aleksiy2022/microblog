30c21ec7cc0d9eef97b7382c4ea1561ac15e6102
kashmir <kuznetcov.alekcey@icloud.com>
2024-05-29 19:44:18 +0300

    Added dependencies tests.

---
+++ tests/test_twitter_api/test_dependencies.py
@@ -0,0 +1,36 @@
+from typing import AsyncGenerator
+import pytest
+from twitter_api import dependencies
+from twitter_api.core import test_db_helper
+from fastapi import HTTPException
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_scoped_session_db():
+    session = dependencies.scoped_session_db()
+    assert isinstance(session, AsyncGenerator)
+
+
+@pytest.mark.parametrize(
+    "api_key, exp_err_msg",
+    [
+        ("test", None),
+        ("some_wrong_api_key", "User with api_key: some_wrong_api_key not found"),
+    ]
+)
+@pytest.mark.asyncio(scope="session")
+async def test_get_current_user_by_api_key(api_key, exp_err_msg):
+    session = test_db_helper.get_scoped_session()
+    if exp_err_msg:
+        with pytest.raises(HTTPException) as excinfo:
+            await dependencies.get_current_user_by_api_key(
+                session=session,
+                api_key=api_key
+            )
+        assert exp_err_msg in str(excinfo.value)
+    else:
+        user = await dependencies.get_current_user_by_api_key(
+            session=session,
+            api_key=api_key
+        )
+        assert user.id == 1
