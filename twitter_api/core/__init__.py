947cf6f66f6e53a17fc8e8b358a58ae6c50d6d21
kashmir <kuznetcov.alekcey@icloud.com>
2024-05-30 16:10:49 +0300

    Delete DatadaseHelper from import.

--- twitter_api/core/__init__.py
+++ twitter_api/core/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ("db_helper", "DatabaseHelper", "settings", "test_db_helper")
+__all__ = ("db_helper", "settings", "test_db_helper")
 
 from .config import settings
-from .dbhelper import DatabaseHelper, db_helper, test_db_helper
+from .dbhelper import db_helper, test_db_helper
