35618e4ce588a2518b46b28b2bef2a7b4c84a15e
kashmir <kuznetcov.alekcey@icloud.com>
2024-05-30 16:14:25 +0300

    Rewrite func save_tweet_image.

--- twitter_api/routers/utils.py
+++ twitter_api/routers/utils.py
@@ -1,18 +1,27 @@
 import os
 
 import aiofiles  # type: ignore
-from fastapi import UploadFile
-from fastapi.exceptions import RequestValidationError
+from fastapi import HTTPException, UploadFile
 
 from ..core import settings
 
 
 async def save_tweet_image(image: UploadFile) -> str:
+    image_extensions = (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff")
     file_name: str | None = image.filename
-    if file_name is None:
-        raise RequestValidationError(errors=[], body="No file name")
-    file_path = os.path.join(settings.dir_uploaded_images, file_name)
-    async with aiofiles.open(file_path, "wb") as f:
-        content = await image.read()
-        await f.write(content)
-    return os.path.join("tweets_images", file_name)
+
+    if file_name:
+        if file_name.endswith(image_extensions):
+            file_path = os.path.join(settings.dir_uploaded_images, file_name)
+
+            async with aiofiles.open(file_path, "wb") as f:
+                content = await image.read()
+                await f.write(content)
+
+            return os.path.join("tweets_images", file_name)
+        else:
+            raise HTTPException(
+                status_code=415, detail="File type not supported"
+            )
+    else:
+        raise HTTPException(status_code=415, detail="File type not supported")
